d6e49c0721b623287ffd45a27d30ee6ba0977f31
Nils Alexander Haupt <nils.alexander.haupt@uni-jena.de>
2019-11-28 02:26:57 +0100

    Update for create-package.R: package.skeleton is not used anymore

---
+++ client_api_r/start_server.R
@@ -0,0 +1,11 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: Latitude
+# Created on: 27.11.2019
+
+#' This service will be implemented soon.
+#'
+start_server <- function(){
+	setwd("C:\\Users\\Latitude\\Desktop\\sirius-win64-headless-service-4.3.0-SNAPSHOT")
+	system("sirius-console-64.exe -i 'E:\\Bioinformatik (B.Sc. + M.Sc.)\\HiWi\\mice_mzml_500d' asService")
+}
