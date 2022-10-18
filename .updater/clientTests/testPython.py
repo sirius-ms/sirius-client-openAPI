b549b9e99bc0b1325a49b58438433da12afba583
Lukas Scholz <l.kas.scholz@gmail.com>
2022-10-18 14:12:46 +0200

    Update testPython.py

--- .updater/clientTests/testPython.py
+++ .updater/clientTests/testPython.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import time
-import swagger_client
+import PySirius
 
 
 #### credentials for the login ####
@@ -15,37 +15,38 @@ pw = ""
 
 
 # get config
-api = swagger_client.ComputationsApi()
+api = PySirius.ComputationsApi()
 config = api.get_default_job_config()
 print("Config Loaded Successfull!")
 print()
 
 # test basic stuff
 print("Testing general stuff...")
-api = swagger_client.VersionInfoControllerApi()
+api = PySirius.VersionInfoControllerApi()
 print("Version:")
 print(api.get_version_info()) # check for version
 print()
-api = swagger_client.ProjectSpacesApi()
+api = PySirius.ProjectSpacesApi()
 print("Project Spaces")
 print(api.get_project_spaces()) # check for project spaces
 print()
 
+## TODO -- Logins ##
 # check if user is logged in
-api = swagger_client.LoginAndAccountApi()
-if(api.is_logged_in()):
-	pass
-else:
-	if((name == "") & (pw == "")):	
-		print("Please Create an account in via the following link and add the credentials to this file")
-		print(api.get_sign_up_url())
-		exit(0)
-			
-	else:
-		body = swagger_client.AccountCredentials(username=name, password=pw)
-		print("Logging in...")
-		api.login(body, True)
-print()
+#api = PySirius.LoginAndAccountApi()
+#if(api.is_logged_in()):
+#	pass
+#else:
+#	if((name == "") & (pw == "")):	
+#		print("Please Create an account in via the following link and add the credentials to this file")
+#		print(api.get_sign_up_url())
+#		exit(0)
+#			
+#	else:
+#		body = PySirius.AccountCredentials(username=name, password=pw)
+#		print("Logging in...")
+#		api.login(body, True)
+#print()
 
 # user is logged in
 print("Testing user specific stuff")
