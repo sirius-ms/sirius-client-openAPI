2f589b5158e0c0fda3f9edffb19f30ced502ddc9
Lukas Scholz <l.kas.scholz@gmail.com>
2022-10-30 13:17:37 +0100

    Update testPython.py

--- .updater/clientTests/testPython.py
+++ .updater/clientTests/testPython.py
@@ -11,16 +11,16 @@ from PySirius import SiriusAPI
 
 
 # get config
-SiriusAPI.ComputationsApi().get_default_job_config()
+SiriusAPI.ComputationsApi.get_default_job_config()
 print("Config Loaded Successfull!")
 print()
 
 # test basic stuff
 print("Version:")
-print(SiriusAPI.VersionInfoControllerApi().get_version_info()) # check for version
+print(SiriusAPI.VersionInfoControllerApi.get_version_info()) # check for version
 print()
 print("Project Spaces")
-print(SiriusAPI.ProjectSpacesApi().get_project_spaces()) # check for project spaces
+print(SiriusAPI.ProjectSpacesApi.get_project_spaces()) # check for project spaces
 print()
 
 print("Testing user specific stuff")
