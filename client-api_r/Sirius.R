cb3a46becb01114bb74486ae0e04b07dd05de603
joXemMx <63006948+joXemMx@users.noreply.github.com>
2022-12-06 20:32:10 +0100

    Create Sirius.R

---
+++ client-api_r/Sirius.R
@@ -0,0 +1,166 @@
+library(R6)
+library(R6P)
+library(ps)
+library(tools)
+library(httr2)
+library(dplyr)
+
+Sirius = R6::R6Class(
+  classname = "Sirius",
+  inherit = R6P::Singleton,
+  public = list(
+    
+    pid = NULL,
+    port = NULL,
+    host = NULL,
+    basePath = NULL,
+    
+    start = function(host = "http://localhost:", port = 8080, pathToSirius, projectSpace = ""){
+      
+      if(!is.null(self$pid)){
+        stop(paste("Sirius has already been started with PID: ",self$pid, sep = ""))
+      }
+      
+      if(is.character(host) && length(host) == 1){
+        if(is.numeric(port)){
+          self$port <- as.integer(port)
+          self$host <- host
+          
+          self$basePath <- paste(self$host,self$port, sep = "")
+        }else{
+          stop("The given parameter \"port\" has to be an integer value.")
+        }
+      }else{
+        stop("The given parameter \"host\" has to be a valid domain name.
+                \"host\" has to be a character vector of length one.")
+      }
+      
+      if(all(is.character(pathToSirius),length(pathToSirius) == 1)){
+        if(all(file.exists(pathToSirius),!dir.exists(pathToSirius))){
+
+          sirius <- basename(pathToSirius) # the file which has to be executed
+          dir_sirius <- dirname(pathToSirius)
+          # Change working directory to the directory which contains SIRIUS.
+          # This has to be done in the case that pathToSirius contains at least one whitespace.
+          wd <- getwd()
+          setwd(dir_sirius)
+            
+          # It is also possible that inputData and projectSpace contain at least one whitespace:
+          sirius_call <- paste("./",sirius, sep = "")
+            
+          if(!missing(projectSpace)){
+            if(all(is.character(projectSpace),length(projectSpace) == 1)){
+              if(file.exists(projectSpace)){
+                sirius_call <- paste(sirius_call," --project-space=","\"",projectSpace,"\"",sep = "")
+              }else{
+                stop("The string \"projectSpace\" should represent a valid path to your project space.")
+              }
+            }else{
+              stop("The given parameter \"projectSpace\" has to be a character vector of length 1.")
+            }
+          }
+          sirius_call <- paste(sirius_call," rest -s -p ",self$port,sep = "")
+          print(sirius_call)
+          # Call SIRIUS as background service in commando line:
+          system(sirius_call, wait=FALSE)
+          print  
+          Sys.sleep(10) #delete later 
+          # MAY NEED TO CHANGE WD
+          #for (i in 1:6){
+          #  Sys.sleep(5)
+          #  if(file.exists(PID.FILE)){
+          #    self$pid <- PID.FROM.FILE
+          #    break
+          #  }
+          #}
+          #todo: SET PID HERE
+          setwd(wd)
+          #return(ApiClient(self$basePath))
+        }else{
+          stop("The given string \"pathToSirius\" should represent a valid path to the executable SIRIUS file.")
+        }
+      }else{
+        stop("The parameter \"pathToSirius\" does not meet the requirements.
+		            It has to be a character vector of length 1.")
+      }
+    },
+  
+    is_active = function(){
+      tryCatch(
+        {
+          req <- request(paste(self$basePath,"/actuator/health", sep = ""))
+          resp <- req_perform(req)
+          if(resp_status(resp) >= 200 && resp_status(resp) <= 299){
+            if(resp_body_json(resp) == "UP"){
+              return(TRUE)
+            }else{
+              return(FALSE)
+            }
+          }else{
+            print("The request was not completely successfull.")
+            return(FALSE)
+          }
+        },error = function(e){
+          return(FALSE)
+        })
+    },
+    
+    shutdown = function(){
+      if(self$is_active()){
+        req_shutdown = req_method(request(paste(self$basePath,"/actuator/shutdown",sep = "")), "POST")
+        resp_shutdown = req_perform(req_shutdown)
+        if(resp_body_json(resp_shutdown)=="Shutting down SpringBootApp and SIRIUS afterward, bye..."){
+          print("The SIRIUS REST service ended successfully. ")
+          self$pid=NULL
+        } else {
+          print("SIRIUS REST service seems not to have shut down as intended.")
+        }
+        
+        if(Sys.info()['sysname']=="Linux"){
+          print("Trying to end Sirius via SIGTERM...")
+          pskill(PID, SIGTERM)
+          Sys.sleep(5)
+          
+          if(file.exists(PID.FILE)){
+            print("SIGTERM did not work. Trying to end Sirius via SIGKILL...")
+            pskill(PID, SIGKILL)
+            Sys.sleep(5)
+            
+            if(file.exists(PID.FILE)){
+              print("SIGKILL did not work. Please shut down your Port running Sirius and delete the PID.FILE.")
+            } else {
+              print("The SIRIUS REST service ended successfully. ")
+            }
+          } else {
+            print("The SIRIUS REST service ended successfully. ")
+          }
+          
+        } else {
+          print("Trying to end Sirius via SIGINT...")
+          pskill(PID, SIGINT)
+          Sys.sleep(5)
+          
+          if(file.exists(PID.FILE)){
+            print("SIGINT did not work. Trying to end Sirius via SIGTERM...")
+            pskill(PID, SIGTERM)
+            Sys.sleep(5)
+            
+            if(file.exists(PID.FILE)){
+              print("SIGTERM did not work. Please shut down your Port running Sirius and delete the PID.FILE.")
+            } else {
+              print("The SIRIUS REST service ended successfully. ")
+            }
+          } else {
+            print("The SIRIUS REST service ended successfully. ")
+          }
+        }
+      }else{
+        print("SIRIUS does not run as REST service at this moment.")
+      }
+    },
+    
+    getApiClient = function(host = self$host, port = self$port){
+      return(ApiClient$new(paste(host,port, sep = "")))
+    }
+  )
+)
