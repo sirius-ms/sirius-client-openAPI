ce948aecd7926184829ddb4164fa09451435f9de
Lukas Scholz <l.kas.scholz@gmail.com>
2022-12-14 16:13:59 +0100

    Update sirius_api.py

--- client-api_python/generated/PySirius/sirius_api.py
+++ client-api_python/generated/PySirius/sirius_api.py
@@ -7,14 +7,6 @@ import json
 import urllib3
 import PySirius
 
-from PySirius.apis.tags import compounds_api
-from PySirius.apis.tags import computations_api
-from PySirius.apis.tags import formula_results_api
-from PySirius.apis.tags import graphical_user_interface_api
-from PySirius.apis.tags import login_and_account_api
-from PySirius.apis.tags import project_spaces_api
-from PySirius.apis.tags import version_info_controller_api
-
 class PySiriusAPI:
 
     def __init__(self, address="http://localhost", port=8080):
@@ -24,16 +16,16 @@ class PySiriusAPI:
         self.api_client = PySirius.ApiClient(configuration)
     
     def get_CompoundsApi(self):
-        return compounds_api.CompoundsApi(self.api_client)
+        return PySirius.CompoundsApi(self.api_client)
     def get_ComputationsApi(self):
-        return computations_api.ComputationsApi(self.api_client)
+        return PySirius.ComputationsApi(self.api_client)
     def get_FormulaResultsApi(self):
-        return formula_results_api.FormulaResultsApi(self.api_client)
+        return PySirius.FormulaResultsApi(self.api_client)
     def get_GraphicalUserInterfaceApi(self):
-        return graphical_user_interface_api.GraphicalUserInterfaceApi(self.api_client)
+        return PySirius.GraphicalUserInterfaceApi(self.api_client)
     def get_LoginAndAccountApi(self):
-        return login_and_account_api.LoginAndAccountApi(self.api_client)
+        return PySirius.LoginAndAccountApi(self.api_client)
     def get_ProjectSpacesApi(self):
-        return project_spaces_api.ProjectSpacesApi(self.api_client)
+        return PySirius.ProjectSpacesApi(self.api_client)
     def get_VersionInfoControllerApi(self):
-        return version_info_controller_api.VersionInfoControllerApi(self.api_client)
+        return PySirius.VersionInfoControllerApi(self.api_client)
