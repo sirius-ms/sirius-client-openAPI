4e74f14dd31dba3aa2cb98bbce5529eb55ebbaa8
Lukas Scholz <l.kas.scholz@gmail.com>
2022-12-14 16:27:46 +0100

    fixed base path

--- client-api_python/generated/PySirius/sirius_api.py
+++ client-api_python/generated/PySirius/sirius_api.py
@@ -12,7 +12,8 @@ class PySiriusAPI:
     def __init__(self, address="http://localhost", port=8080):
         self.port = str(port)
         base_path = address + ":" + self.port
-        configuration = PySirius.Configuration(host = base_path)
+        configuration = PySirius.Configuration()
+        configuration.host = base_path
         self.api_client = PySirius.ApiClient(configuration)
     
     def get_CompoundsApi(self):
