927ca4999c25a1d953f314ee816795428dff6b9b
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-06 13:47:39 +0100

    Update conftest.py

--- .updater/clientTests/conftest.py
+++ .updater/clientTests/conftest.py
@@ -5,7 +5,7 @@ def pytest_addoption(parser):
     parser.addoption("--name", action="store")
     parser.addoption("--passw", action="store")
 
-@pytest.fixture
+@pytest.fixture(scope="session", autouse=True)
 def command_line_args(request):
     args = {}
     args['name'] = request.config.getoption('--name')
