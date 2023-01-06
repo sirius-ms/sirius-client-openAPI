a59344abab2ab9d3cbc38323d9c2c8fb86af2b29
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-06 13:57:20 +0100

    Update testPython.py

--- .updater/clientTests/testPython.py
+++ .updater/clientTests/testPython.py
@@ -11,24 +11,28 @@ api = SiriusSDK.start(path_to_project, path_to_sirius)
 ps_name = api.get_ProjectSpacesApi().get_project_spaces()[0].name
 
   
-def setup_module(command_line_args):
-  print(command_line_args)
+def init(command_line_args):
   credentials = {"username":str(command_line_args[0]), "password":str(command_line_args[1])}
   api.get_LoginAndAccountApi().login(credentials, True)
 
-def teardown_module():
+def end():
   api.get_LoginAndAccountApi().logout()
     
-def test_Computations():
+def test_Computations(command_line_args):
+  init(command_line_args)
   test = api.get_ComputationsApi()
+  end()
   assert True
 
-def test_ProjectSpaces():
+def test_ProjectSpaces(command_line_args):
+  init(command_line_args)
   test = api.get_ProjectSpacesApi()
   test.get_project_spaces()
+  end()
   assert True
 
-def test_LoginAndAccounts():
+def test_LoginAndAccounts(command_line_args):
+  init(command_line_args)
   test = api.get_LoginAndAccountApi()
   assert test.is_logged_in()
   assert test.is_logged_in_with_http_info()
@@ -38,30 +42,40 @@ def test_LoginAndAccounts():
   test.get_sign_up_url_with_http_info()
   # test.sign_up()
   # test.sign_up_with_http_info()
+  end()
   assert True
 
-def test_FormulaResults():
+def test_FormulaResults(command_line_args):
+  init(command_line_args)
   test = api.get_FormulaResultsApi()
+  end()
   assert True
 
-def test_Compounds():
+def test_Compounds(command_line_args):
+  init(command_line_args)
   test = api.get_CompoundsApi()
   test.get_compounds(ps_name)
+  end()
   assert True
   
-def test_VersionController():
+def test_VersionController(command_line_args):
+  init(command_line_args)
   test = api.get_VersionInfoControllerApi()
   test.get_version_info()
   test.get_version_info_with_http_info()
+  end()
   assert True
   
-def test_GUI():
+def test_GUI(command_line_args):
+  init(command_line_args)
   test = api.get_GraphicalUserInterfaceApi()
   # test.open_gui()
   # test.close_gui()
+  end()
   assert True
   
-def test_Workflow():
+def test_Workflow(command_line_args):
+  init(command_line_args)
   api.get_CompoundsApi().import_compounds([path_to_demo_data+"/ms/Bicuculline.ms", path_to_demo_data+"/ms/Kaempferol.ms" ], ps_name)
   print(api.get_CompoundsApi().get_compounds(ps_name))  # TODO: Compounds not imported??
   fallback_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+","[M+2K-H]+","[M+NH4]+","[M+H3O]+","[M+MeOH+H]+"]
@@ -71,4 +85,5 @@ def test_Workflow():
   job = models.JobSubmission([api.get_CompoundsApi().get_compounds(ps_name)[0].id, api.get_CompoundsApi().get_compounds(ps_name)[1].id], fallback_adducts, None, detectable_adducts, True, formula_id_paras)
   time.sleep(2)
   api.get_ComputationsApi().start_job(job, ps_name)
+  end()
   assert True
