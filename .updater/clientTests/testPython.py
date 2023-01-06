c6a10ed8a9593fe952e08a902bdd65aedafb3673
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-06 14:13:59 +0100

    Update testPython.py

--- .updater/clientTests/testPython.py
+++ .updater/clientTests/testPython.py
@@ -33,16 +33,16 @@ def teardown_module():
   api.get_ComputationsApi().start_job(job, ps_name)
   assert True
     
-def test_Computations(command_line_args):
+def test_Computations():
   test = api.get_ComputationsApi()
   assert True
 
-def test_ProjectSpaces(command_line_args):
+def test_ProjectSpaces():
   test = api.get_ProjectSpacesApi()
   test.get_project_spaces()
   assert True
 
-def test_LoginAndAccounts(command_line_args):
+def test_LoginAndAccounts():
   test = api.get_LoginAndAccountApi()
   assert test.is_logged_in()
   assert test.is_logged_in_with_http_info()
@@ -54,29 +54,29 @@ def test_LoginAndAccounts(command_line_args):
   # test.sign_up_with_http_info()
   assert True
 
-def test_FormulaResults(command_line_args):
+def test_FormulaResults():
   test = api.get_FormulaResultsApi()
   end()
   assert True
 
-def test_Compounds(command_line_args):
+def test_Compounds():
   test = api.get_CompoundsApi()
   test.get_compounds(ps_name)
   assert True
   
-def test_VersionController(command_line_args):
+def test_VersionController():
   test = api.get_VersionInfoControllerApi()
   test.get_version_info()
   test.get_version_info_with_http_info()
   assert True
   
-def test_GUI(command_line_args):
+def test_GUI():
   test = api.get_GraphicalUserInterfaceApi()
   # test.open_gui()
   # test.close_gui()
   assert True
   
-def test_Workflow(command_line_args):
+def test_Workflow():
   api.get_CompoundsApi().import_compounds([path_to_demo_data+"/ms/Bicuculline.ms", path_to_demo_data+"/ms/Kaempferol.ms" ], ps_name)
   print(api.get_CompoundsApi().get_compounds(ps_name))  # TODO: Compounds not imported??
   fallback_adducts = ["[M+H]+","[M]+,[M+K]+","[M+Na]+","[M+H-H2O]+","[M+Na2-H]+","[M+2K-H]+","[M+NH4]+","[M+H3O]+","[M+MeOH+H]+"]
