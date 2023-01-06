bb3d9601807530523cc6d86497206ed048cd1006
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-06 11:20:01 +0100

    Update conftest.py

--- .updater/clientTests/conftest.py
+++ .updater/clientTests/conftest.py
@@ -5,9 +5,9 @@ def pytest_addoption(parser):
     parser.addoption("--name", action="store")
     parser.addoption("--passw", action="store")
 
-@fixture()
+@pytest.fixture()
 def name(request):
     return request.config.getoption("--name")
-@fixture()
+@pytest.fixture()
 def passw(request):
   return request.config.getoption("--passw")
