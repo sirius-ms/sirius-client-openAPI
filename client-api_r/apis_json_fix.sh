f5e23bab2cef7d6451eda86c1bb74d8bcc6d7350
Jonas Emmert <63006948+joXemMx@users.noreply.github.com>
2023-01-12 22:11:46 +0100

    Create apis_json_fix.sh

---
+++ client-api_r/apis_json_fix.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Check if the input file is given as an argument
+if [ $# -eq 0 ]; then
+  echo "Error: No input file specified."
+  exit 1
+fi
+
+# Check if the number of arguments is correct
+if [ "$#" -ne 1 ]; then
+    echo "Error: Illegal number of arguments. Please specify the path to the R file."
+    exit 1
+fi
+
+# Check if the input file exists
+if [ ! -f $1 ]; then
+  echo "Error: Input file does not exist."
+  exit 1
+fi
+
+
+# fix simple local_var_body
+sed -i 's/^\([[:blank:]]*\)local_var_body <- `job_submission`$toJSONString()/\1local_var_body <- jsonlite::toJSON(job_submission)/' $1
+
+## fix multi-row if statement
+# delete lines
+sed -i '/^[[:blank:]]*if (!is.null(`request_body`)) {/{n;N;N;N;d}' $1
+#insert new line with fitting indentation
+sed -i 's/^\([[:blank:]]*\)if (!is.null(`request_body`)) {/\1if (!is.null(`request_body`)) {\n\1  local_var_body <- jsonlite::toJSON(request_body)/' $1
