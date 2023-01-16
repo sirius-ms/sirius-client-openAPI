f97510c1029b8bc2e5ea5afe7707b573b3c9af02
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-16 13:41:50 +0100

    adde empty function test classes

--- .updater/clientTests/PythonTests/PythonTestAutoFix.sh
+++ .updater/clientTests/PythonTests/PythonTestAutoFix.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm ./../../../client-api_python/generated/test/*
-cp ./test/* ./../../../client-api_python/generated/test/
+cp ./test/* ./../../../client-api_python/generated/test/test_*.py
 echo "omitting directory ./test/models is expected!"
-cp ./test/models/* ./../../../client-api_python/generated/test/
+cp ./test/models/* ./../../../client-api_python/generated/test/test_*.py
 echo "Done!"
