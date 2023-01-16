49617be32f20e11936e6158c3eb97dc72bf5aea4
Lukas Scholz <l.kas.scholz@gmail.com>
2023-01-16 16:09:38 +0100

    Update PythonTestAutoFix.sh

--- .updater/clientTests/PythonTests/PythonTestAutoFix.sh
+++ .updater/clientTests/PythonTests/PythonTestAutoFix.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 rm ./../../../client-api_python/generated/test/*
-cp ./test/* ./../../../client-api_python/generated/test/test_*.py
+cp ./test/test_*.py ./../../../client-api_python/generated/test
 echo "omitting directory ./test/models is expected!"
-cp ./test/models/* ./../../../client-api_python/generated/test/test_*.py
+cp ./test/models/test_*.py ./../../../client-api_python/generated/test
 echo "Done!"
