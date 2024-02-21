c154d16f09e05e1b0756a46ce9ba3cf709e9db72
MartinHoffmannJena <43675767+MartinHoffmannJena@users.noreply.github.com>
2024-02-21 16:07:08 +0100

    Update sirius_api.py
    Updated API Endpoints for S6

--- client-api_python/sirius_api.py
+++ client-api_python/sirius_api.py
@@ -1,3 +1,5 @@
+#THIS FILE IS NOT AUTO GENERATED AND MIGHT NEED TO BE CHANGED WHEN API ENDPOINTS CHANGE
+
 import PySirius
 
 class PySiriusAPI:
@@ -13,24 +15,30 @@ class PySiriusAPI:
     def get_CompoundsApi(self):
         """returns API endpoint of the CompoundsApi"""
         return PySirius.CompoundsApi(self.api_client)
-    def get_ComputationsApi(self):
-        """returns API endpoint of the ComputationsApi"""
-        return PySirius.ComputationsApi(self.api_client)
-    def get_FormulaResultsApi(self):
-        """returns API endpoint of the FormulaResultsApi"""
-        return PySirius.FormulaResultsApi(self.api_client)
-    def get_GraphicalUserInterfaceApi(self):
-        """returns API endpoint of the GraphicalUserInterfaceApi"""
-        return PySirius.GraphicalUserInterfaceApi(self.api_client)
+    def get_JobsApi(self):
+        """returns API endpoint of the JobsApi"""
+        return PySirius.JobsApi(self.api_client)
+    def get_FeaturesApi(self):
+        """returns API endpoint of the FeaturesApi"""
+        return PySirius.FeaturesApi(self.api_client)
+    def get_CompoundsApi(self):
+        """returns API endpoint of the CompoundsApi"""
+        return PySirius.CompoundsApi(self.api_client)
+    def get_ExperimentalGUIApi(self):
+        """returns API endpoint of the ExperimentalGUIApi"""
+        return PySirius.ExperimentalGUIApi(self.api_client)
     def get_LoginAndAccountApi(self):
         """returns API endpoint of the LoginAndAccountApi"""
         return PySirius.LoginAndAccountApi(self.api_client)
-    def get_ProjectSpacesApi(self):
-        """returns API endpoint of the ProjectSpacesApi"""
-        return PySirius.ProjectSpacesApi(self.api_client)
-    def get_VersionInfoControllerApi(self):
-        """returns API endpoint of the VersionInfoControllerApi"""
-        return PySirius.VersionInfoControllerApi(self.api_client)
+    def get_ProjectsApi(self):
+        """returns API endpoint of the ProjectsApi"""
+        return PySirius.ProjectsApi(self.api_client)
+    def get_InfoApi(self):
+        """returns API endpoint of the InfoApi"""
+        return PySirius.InfoApi(self.api_client)
+    def get_ActuatorApi(self):
+        """returns API endpoint of the ActuatorApi"""
+        return PySirius.ActuatorApi(self.api_client)
     def get_models(self):
         """returns the Superclass of all models"""
         return PySirius.models
