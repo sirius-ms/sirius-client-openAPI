afd6bdc3948cf95d8bf7066299b3bcc55e45f430
boeckergitlab <44767363+boeckergitlab@users.noreply.github.com>
2024-03-22 16:59:06 +0100

    Auto-Updated OpenAPI Clients (#79)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- client-api_r/generated/R/formula_candidate_opt_field.R
+++ client-api_r/generated/R/formula_candidate_opt_field.R
@@ -68,8 +68,7 @@ FormulaCandidateOptField <- R6::R6Class(
     #' @return FormulaCandidateOptField in JSON format
     #' @export
     toJSONString = function() {
-      as.character(jsonlite::toJSON(private$value,
-          auto_unbox = TRUE))
+      jsonlite::toJSON(private$value, auto_unbox = TRUE)
     },
     #' Deserialize JSON string into an instance of FormulaCandidateOptField
     #'
