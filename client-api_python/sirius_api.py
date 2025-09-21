c5558eb94c757b1b71fb60ff8fb154a81fa5c54d
joXemMx <jonas.em@t-online.de>
2025-09-21 17:36:38 +0200

    add all missing apis

--- client-api_python/sirius_api.py
+++ client-api_python/sirius_api.py
@@ -8,25 +8,37 @@ class PySiriusAPI:
 
     def __init__(self, api_client):
         self.api_client = api_client
-    
+
     def get_client(self):
         return self.api_client
+
+    def actuator(self):
+        """returns API endpoint of the ActuatorApi"""
+        return PySirius.ActuatorApi(self.api_client)
+
+    def compound_statistics(self):
+        """returns API endpoint of the CompoundsStatisticsApi"""
+        return PySirius.CompoundStatisticsApi(self.api_client)
 
     def compounds(self):
         """returns API endpoint of the CompoundsApi"""
         return PySirius.CompoundsApi(self.api_client)
 
-    def jobs(self):
-        """returns API endpoint of the JobsApi"""
-        return PySirius.JobsApi(self.api_client)
+    def feature_statistics(self):
+        """returns API endpoint of the FeaturesStatisticsApi"""
+        return PySirius.FeatureStatisticsApi(self.api_client)
 
     def features(self):
         """returns API endpoint of the FeaturesApi"""
         return PySirius.FeaturesApi(self.api_client)
 
-    def gui(self):
-        """returns API endpoint of the ExperimentalGUIApi"""
-        return PySirius.GuiApi(self.api_client)
+    def infos(self):
+        """returns API endpoint of the InfoApi"""
+        return PySirius.InfoApi(self.api_client)
+
+    def jobs(self):
+        """returns API endpoint of the JobsApi"""
+        return PySirius.JobsApi(self.api_client)
 
     def account(self):
         """returns API endpoint of the LoginAndAccountApi"""
@@ -36,13 +48,17 @@ class PySiriusAPI:
         """returns API endpoint of the ProjectsApi"""
         return PySirius.ProjectsApi(self.api_client)
 
-    def infos(self):
-        """returns API endpoint of the InfoApi"""
-        return PySirius.InfoApi(self.api_client)
+    def runs(self):
+        """returns API endpoint of the RunsApi"""
+        return PySirius.RunsApi(self.api_client)
 
     def databases(self):
         """returns API endpoint of the SearchableDatabasesApi"""
         return PySirius.SearchableDatabasesApi(self.api_client)
+
+    def tags(self):
+        """returns API endpoint of the TagsApi"""
+        return PySirius.TagsApi(self.api_client)
 
     def models(self):
         """returns the Superclass of all models"""
