8adbd31da083990d6be657abf4e0412e128f3e81
joXemMx <jonas.em@t-online.de>
2025-12-09 14:48:41 +0100

    remove experimental endpoint modules from pysirius_api

--- client-api_python/pysirius_api.py
+++ client-api_python/pysirius_api.py
@@ -20,17 +20,9 @@ class PySiriusAPI:
         """returns API endpoint of the ActuatorApi"""
         return PySirius.ActuatorApi(self.api_client)
 
-    def compound_statistics(self):
-        """returns API endpoint of the CompoundsStatisticsApi"""
-        return PySirius.CompoundStatisticsApi(self.api_client)
-
     def compounds(self):
         """returns API endpoint of the CompoundsApi"""
         return PySirius.CompoundsApi(self.api_client)
-
-    def feature_statistics(self):
-        """returns API endpoint of the FeaturesStatisticsApi"""
-        return PySirius.FeatureStatisticsApi(self.api_client)
 
     def features(self):
         """returns API endpoint of the FeaturesApi"""
@@ -52,17 +44,9 @@ class PySiriusAPI:
         """returns API endpoint of the ProjectsApi"""
         return PySirius.ProjectsApi(self.api_client)
 
-    def runs(self):
-        """returns API endpoint of the RunsApi"""
-        return PySirius.RunsApi(self.api_client)
-
     def databases(self):
         """returns API endpoint of the SearchableDatabasesApi"""
         return PySirius.SearchableDatabasesApi(self.api_client)
-
-    def tags(self):
-        """returns API endpoint of the TagsApi"""
-        return PySirius.TagsApi(self.api_client)
 
     def models(self):
         """returns the Superclass of all models"""
